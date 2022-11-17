afb2778941af1a0cae5f36af4d9d08985c7594a3
joemarini <cjmarini68@gmail.com>
2022-11-17 10:07:52 -0800

    Initial commit

---
+++ Finished/Ch_1/minmax.py
@@ -0,0 +1,32 @@
+# Example file for Advanced Python: Working With Data by Joe Marini
+# Demonstrates the usage of the min and max functions
+import json
+
+# Declare an array with some sample data in it
+values = [3.0, 2.5, 5.1, 4.1, 1.8, 1.6, 2.2, 5.7, 6.1]
+strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
+
+# The min() function finds the minimum value
+print(f"The minimum value is: {min(values)}")
+print(f"The minimum value is: {min(strings,key=len)}")
+
+# The max() function finds the maximum value
+print(f"The minimum value is: {max(values)}")
+print(f"The minimum value is: {max(strings,key=len)}")
+
+# define a custom "key" function to extract a data field
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+
+# open the data file and load the JSON
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+print(data["metadata"]["title"])
+print(len(data["features"]))
+print(min(data["features"], key=getmag))
+print(max(data["features"], key=getmag))
---
+++ Start/Ch_1/minmax.py
@@ -0,0 +1,2 @@
+# Example file for Advanced Python: Working With Data by Joe Marini
+# Demonstrates the usage of the min and max functions
