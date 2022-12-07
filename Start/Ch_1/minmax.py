f4b585da8c78688dbac771a293fc404dab084cf8
joemarini <cjmarini68@gmail.com>
2022-12-06 23:34:51 -0800

    Add TODO comments

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -1,2 +1,13 @@
 # Example file for Advanced Python: Working With Data by Joe Marini
 # Demonstrates the usage of the min and max functions
+
+# TODO: The min() function finds the minimum value
+
+
+# TODO: The max() function finds the maximum value
+
+
+# TODO: define a custom "key" function to extract a data field
+
+
+# TODO: open the data file and load the JSON
