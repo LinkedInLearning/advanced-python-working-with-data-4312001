cfbd544a5b601ecf500e06e35d9ccb187e6ec62f
joemarini <cjmarini68@gmail.com>
2022-12-30 13:11:11 -0800

    fix spacing

--- Finished/Ch_1/challenge_solution.py
+++ Finished/Ch_1/challenge_solution.py
@@ -8,7 +8,7 @@ import json
 # 1: How many quakes are there in total?
 # 2: How many quakes were felt by at least 100 people?
 # 3: Print the name of the place whose quake was felt by the most people, with the # of reports
-# 4: Print the top 10 most significant events, with the significance value of each
+# 4: Print the top 10 most significant events
 
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
@@ -17,30 +17,40 @@ with open("../../30DayQuakes.json", "r") as datafile:
 # 1: We can just use the provided data, or we can use len() to get the length of the "features"
 print(f"Total quakes: {data['metadata']['count']}")
 
+
 # 2: This is a straightforward filtering process
 def feltreport(q):
-  f = q["properties"]["felt"]
-  return (f is not None and f >= 100)
+    f = q["properties"]["felt"]
+    return (f is not None and f >= 100)
+
 
 feltreports = list(filter(feltreport, data["features"]))
 print(f"Total quakes felt by at least 100 people: {len(feltreports)}")
 
+
 # 3: We can use the max function here to find the maximum # of felt reports
 def getfelt(q):
-  f = q["properties"]["felt"]
-  if f is not None: return f
-  return 0
+    f = q["properties"]["felt"]
+    if f is not None:
+        return f
+    return 0
+
 
 mostfeltquake = max(data["features"], key=getfelt)
-print(f"Most felt reports: {mostfeltquake['properties']['title']}, reports: {mostfeltquake['properties']['felt']}")
+print(
+    f"Most felt reports: {mostfeltquake['properties']['title']}, reports: {mostfeltquake['properties']['felt']}")
+
 
 # 4: This is a sorting operation
 def getsig(q):
-  s = q["properties"]["sig"]
-  if s is not None: return s
-  return 0
+    s = q["properties"]["sig"]
+    if s is not None:
+        return s
+    return 0
+
 
 sigevents = sorted(data["features"], key=getsig, reverse=True)
 print("The 10 most significant events were:")
-for i in range(0,10):
-  print(f"Event: {sigevents[i]['properties']['title']}, Significance: {sigevents[i]['properties']['sig']}")
+for i in range(0, 10):
+    print(
+        f"Event: {sigevents[i]['properties']['title']}, Significance: {sigevents[i]['properties']['sig']}")
