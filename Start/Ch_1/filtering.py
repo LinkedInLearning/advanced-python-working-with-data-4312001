5e9a37d6884a784172e56c46a965fe3b4507ec66
joemarini <cjmarini68@gmail.com>
2022-12-30 11:39:59 -0800

    add filter and transform start states

---
+++ Start/Ch_1/filtering.py
@@ -0,0 +1,32 @@
+# Example file for Advanced Python: Working With Data by Joe Marini
+# using the filter() function to filter a data set
+
+import json
+
+
+def filterEvens(x):
+    # filters out even numbers and keeps odd numbers
+    if x % 2 == 0:
+        return False
+    return True
+
+
+def filterUppers(x):
+    # filters out upper-case letters and keeps lower case letters
+    if x.isupper():
+        return False
+    return True
+
+
+# define some sample sequences to operate on
+nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
+chars = "abcDeFGHiJklmnoP"
+
+# TODO: use filter to remove items from a list
+
+# TODO: use filter on non-numeric sequence
+
+# Use the filter on our data - let's filter out all seismic events that were *not* quakes
+# open the data file and load the JSON
+# with open("../../30DayQuakes.json", "r") as datafile:
+#     data = json.load(datafile)
---
+++ Start/Ch_1/transform.py
@@ -0,0 +1,40 @@
+# Example file for Advanced Python: Working With Data by Joe Marini
+# using the map() function to transform data to another form
+
+import json
+import pprint
+
+
+def squareFunc(x):
+    return x**2
+
+
+def toGrade(x):
+    if (x >= 90):
+        return "A"
+    elif (x >= 80 and x < 90):
+        return "B"
+    elif (x >= 70 and x < 80):
+        return "C"
+    elif (x >= 65 and x < 70):
+        return "D"
+    return "F"
+
+
+# define some sample sequences to operate on
+nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
+grades = (81, 89, 94, 78, 61, 66, 99, 74)
+
+# TODO: use map to create a new sequence of values
+
+# TODO: use sorted and map to change numbers to grades
+
+# Use the filter on our data - let's filter out all seismic events that were *not* quakes
+# open the data file and load the JSON
+# with open("../../30DayQuakes.json", "r") as datafile:
+#     data = json.load(datafile)
+
+
+# TODO: filter the data down to the largest events
+
+# TODO: transform the largest events into a simpler structure
