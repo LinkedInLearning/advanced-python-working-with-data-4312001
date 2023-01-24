6738aa8b26c999c7833fc1923ec56e39a1239b7c
Joe Marini <cjmarini68@gmail.com>
2023-01-24 23:58:32 +0000

    Add deserialize start point

---
+++ Start/Ch_3/deserialize.py
@@ -0,0 +1,14 @@
+# Example file for Advanced Python: Working With Data by Joe Marini
+# read data from a CSV file into an object structure
+
+import csv
+import pprint
+
+
+# read the contents of a CSV file into an object structure
+result = []
+
+# TODO: open the CSV file for reading
+
+
+pprint.pp(result)
