1b9054935b08428ce1294b89452c5ded70d7f7e9
Joe Marini <cjmarini68@gmail.com>
2023-01-24 22:56:21 +0000

    start state for dict

--- Start/Ch_2/defaultdict.py
+++ Start/Ch_2/defaultdict.py
@@ -8,9 +8,10 @@ fruits = ['apple', 'pear', 'orange', 'banana',
           'apple', 'grape', 'banana', 'banana']
 
 # TODO: use a dictionary to count each element
-
+fruitCounter = dict()
 
 # TODO: Count the elements in the list
-
+for fruit in fruits:
+    fruitCounter[fruit] += 1
 
 # TODO: print the result
