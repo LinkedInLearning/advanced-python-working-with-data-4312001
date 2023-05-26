df89ddab7326e15e7ec0f8214afa218b3fa71317
Alberto A. Medina <aa.medina@gmail.com>
2023-05-26 05:02:01 +0000

    Finished Chapter #1.

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -9,14 +9,30 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-
+# print(f"The minimum value is: {min(values)}")
+# print(f"The minimum value is: {min(strings)}")
 
 # TODO: The max() function finds the maximum value
-
+# print(f"The minimum value is: {max(values)}")
+# print(f"The minimum value is: {max(strings)}")
 
 # TODO: define a custom "key" function to extract a data field
-
+# print(f"The minimum value is: {min(strings, key=len)}")
+# print(f"The minimum value is: {max(strings, key=len)}")
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+    print(data["metadata"]["title"])
+    print(len(data["features"]))
+
+
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+
+print(min(data["features"], key=getmag))
+print(max(data["features"], key=getmag))
--- Start/Ch_1/utility.py
+++ Start/Ch_1/utility.py
@@ -5,25 +5,29 @@ import json
 values = [0, 1, 2, 3, 4, 5]
 
 # TODO: any() can be used to see if any value in a sequence is True
-
+# print(any(values))
 
 # TODO: all() will detect if all of the values in a sequence are True
-
+# print(all(values))
 
 # TODO: sum() can be use to add all of the values in a sequence
-
+# print(any(values))
 
 # these utility functions don't have callbacks like min or max,
 # but we can use a generator for more fine control
 
 # open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
 
 # TODO: are there any quake reports that were felt by more than 25,000 people?
-
+print(any(quake["properties"]["felt"] is not None and quake["properties"]["felt"] > 25000
+          for quake in data["features"]))
 
 # TODO: how many quakes were felt by more than 500 people?
-
+print(sum(quake["properties"]["felt"] is not None and quake["properties"]["felt"] > 500
+          for quake in data["features"]))
 
 # TODO: how many quakes had a magnitude of 6 or larger?
+print(sum(quake["properties"]["mag"] is not None and quake["properties"]["mag"] >= 6.0
+          for quake in data["features"]))
