c8244bc8668808633dbbfaee54f42f7a585f5012
Alberto A. Medina <aa.medina@gmail.com>
2023-05-31 23:12:53 +0000

    Finished Chapter #2

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,17 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+from collections import defaultdict
 
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+# Print type of event and the number of events for that type.
+totals = defaultdict(int)
+for event in data['features']:
+    totals[event['properties']['type']] +=1
+
+
+for k,v in totals.items():
+    print(f"{k:15}: {v}")
