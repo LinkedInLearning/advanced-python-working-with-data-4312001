1dd8258200d22c273082df636969438644371bdc
Kamyar Maham <76256654+lioneraV2002@users.noreply.github.com>
2023-07-13 15:58:36 +0000

    new changes made. still learning

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -9,14 +9,30 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-
+print(f"the minimum value is: {min(values)}")
+print(f"the minimum value is: {min(strings)}")
 
 # TODO: The max() function finds the maximum value
-
+# print(f"the maximum value is: {max(values)}")
+# print(f"the maximum value is: {max(strings)}") # order alphabetically
 
 # TODO: define a custom "key" function to extract a data field
-
+# print(f"the maximum value is: {min(strings, key=len)}")
+# print(f"the maximum value is: {max(strings, key=len)}") # by alphabetical order
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+print(data["metadata"]["title"])
+print(len(data["features"]))
+
+
+def getmag(dataitem):
+    magnitute = dataitem["properties"]["mag"]
+    if magnitute is None:
+        magnitute = 0
+    return float(magnitute)
+
+
+
+print(min(data["features"], key=getmag))
