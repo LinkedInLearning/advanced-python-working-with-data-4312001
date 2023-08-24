83c56e82fe65d7d34f8eb023b7aab2d428285b75
Shreevani Rao <123049057+ShreevaniRao@users.noreply.github.com>
2023-08-23 23:40:42 -0500

    Update challenge_start.py

--- Start/Ch_1/challenge_start.py
+++ Start/Ch_1/challenge_start.py
@@ -11,6 +11,31 @@ import json
 # 4: Print the top 10 most significant events, with the significance value of each
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("C:/Users/Vani/Downloads/advanced-python-working-with-data-4312001-main/advanced-python-working-with-data-4312001-main/30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
     
+ #1
+    print(len(data["features"]))
+#2
+    print(sum(quake["properties"]["felt"] is not None and quake["properties"]["felt"] >= 100 for quake in data["features"]))
+#3
+
+    def quakeFelt(quake):
+        if quake["properties"]["felt"] is not None :
+            return True
+        return False
+    
+    quakeFeltbyMost = max(data["features"], key=quakeFelt)
+    print(quakeFeltbyMost["properties"]["title"])
+    print(quakeFeltbyMost["properties"]["felt"])
+  
+#4
+    def getsig(quake):
+        s = quake["properties"]["sig"]
+        if s is not None:
+            return s
+        return 0
+    
+    sigevents = sorted(data["features"], key=getsig, reverse=True)
+    for i in range (0,10):
+        print(f"Event :{sigevents[i]['properties']['title']}, significance- {sigevents[i]['properties']['sig']}")
